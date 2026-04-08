ef25c84a568cbbee41bdc48ce202dd7dca7b6988
AKSHAY BADSHAH <150349402+AKSHAY-BADSHAH@users.noreply.github.com>
2026-04-08 17:48:51 +0530

    Add files via upload

---
+++ app/server.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI
+from app.env import EmailEnv
+from app.models import Action
+
+app = FastAPI()
+
+env = EmailEnv()
+
+# ===============================
+# RESET
+# ===============================
+@app.post("/reset")
+def reset():
+    obs = env.reset()
+    return {"observation": obs.dict()}
+
+# ===============================
+# STEP
+# ===============================
+@app.post("/step")
+def step(action: Action):
+    obs, reward, done, info = env.step(action)
+    return {
+        "observation": obs.dict(),
+        "reward": reward,
+        "done": done,
+        "info": info
+    }
+
+# ===============================
+# STATE
+# ===============================
+@app.get("/state")
+def state():
+    return env.state()
